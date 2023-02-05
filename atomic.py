2c0890cb1dd3bb9f734fe34236a83c696ddd8f8f
Thomas Sturm <sturm@mpi-inf.mpg.de>
2023-02-05 10:13:20 +0100

    New module atomic, AND/OR flatten now
    Moved AtomicFormulas other than Eq from module formula to a new module atomic.
    Do not use those moved AtomicFormulas in the docstrings anymore.
    Added a docstring for the module formula.
    AND and OR produce flattened formulas now.
    Modified latex() rendering for not.

---
+++ atomic.py
@@ -0,0 +1,55 @@
+import sympy
+from .formula import BinaryAtomicFormula
+
+
+class Ne(BinaryAtomicFormula):
+    """
+    >>> Ne(1, 0)
+    Ne(1, 0)
+    """
+    _latex_symbol = "\\neq"
+    _sympy_func = sympy.Ne
+
+    def __init__(self, lhs, rhs):
+        self.func = Ne
+        self.args = (lhs, rhs)
+
+
+class Ge(BinaryAtomicFormula):
+
+    _latex_symbol = "\\geq"
+    _sympy_func = sympy.Ge
+
+    def __init__(self, lhs, rhs):
+        self.func = Ge
+        self.args = (lhs, rhs)
+
+
+class Le(BinaryAtomicFormula):
+
+    _latex_symbol = "\\leq"
+    _sympy_func = sympy.Le
+
+    def __init__(self, lhs, rhs):
+        self.func = Le
+        self.args = (lhs, rhs)
+
+
+class Gt(BinaryAtomicFormula):
+
+    _latex_symbol = ">"
+    _sympy_func = sympy.Gt
+
+    def __init__(self, lhs, rhs):
+        self.func = Gt
+        self.args = (lhs, rhs)
+
+
+class Lt(BinaryAtomicFormula):
+
+    _latex_symbol = "<"
+    _sympy_func = sympy.Lt
+
+    def __init__(self, lhs, rhs):
+        self.func = Lt
+        self.args = (lhs, rhs)
