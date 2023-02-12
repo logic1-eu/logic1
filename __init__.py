490567f298f41d75cdac30fecfbaa757f19b3c50
Thomas Sturm <sturm@mpi-inf.mpg.de>
2023-02-12 11:35:14 +0100

    Add meta information

---
+++ __init__.py
@@ -0,0 +1,7 @@
+__version__ = 0.1
+
+___author___ = 'Thomas Sturm'
+___contact___ = 'https://science.thomas-sturm.de/'
+___copyright__ = 'Copyright 2023, Thomas Sturm, Germany'
+___license__ = 'BSD 2-Clause (aka Simplified BSD, FreeBSD)'
+___status__ = 'Prototype'
