160b0b447b994a16ca432e8eb526e0407b799820
pinguly <faross@chalmers.se>
2025-10-21 13:59:10 +0200

    Working but undocumented computation of sign matrices without parameters

--- logic1/theories/RCF/chm.py
+++ logic1/theories/RCF/chm.py
@@ -1,9 +1,7 @@
+from logic1.theories.RCF import Term, Variable, VV
+from gmpy2 import sign
 from dataclasses import dataclass, field
 from typing import Literal, TypeAlias
-
-from gmpy2 import mpq, sign
-
-from .atomic import Term, Variable
 
 
 Sign: TypeAlias = Literal[-1, 0, 1]
@@ -12,89 +10,35 @@ Sign: TypeAlias = Literal[-1, 0, 1]
 @dataclass
 class Matrix:
 
-    _data: list[list[Sign]] = field(default_factory=list)
+    rows: dict[Term, list[Sign]] = field(default_factory=dict)
 
     @property
-    def cols(self) -> int:
-        if self.rows == 0:
-            return 0
-        return len(self._data[0])
+    def num_rows(self) -> int:
+        return len(self.rows)
 
     @property
-    def rows(self) -> int:
-        return len(self._data)
+    def num_columns(self) -> int:
+        return len(list(self.rows.values())[0]) if self.rows else 0
 
-    def __getitem__(self, indices: tuple[int, int]) -> Sign:
-        i, j = indices
-        return self._data[i][j]
+    def __getitem__(self, f: Term) -> list[Sign]:
+        return list(self.row(f))
 
-    def append(self, row: list[Sign]) -> None:
-        assert len(row) > 0
-        assert self.cols == 0 or self.cols == len(row)
-        self._data.append(row)
+    def row(self, f: Term) -> list[Sign]:
+        return list(self.rows[f])
 
-    def condense(self) -> None:
-        self.transpose()
-        new_data: list[list[Sign]] = []
-        for row in self._data:
-            if not new_data or row != new_data[-1]:
-                new_data.append(row)
-        self._data = new_data
-        self.transpose()
+    def column(self, F: set[Term], i: int) -> dict[Term, sign]:
+        return {f: self.rows[f][i] for f in F}
 
-    def delete(self, col: int) -> None:
-        for row in self._data:
-            del row[col]
+    def insert_constant_row(self, f: Term, s: Sign) -> None:
+        assert f not in self.rows
+        self.rows[f] = [s] * max(1, self.num_columns)
 
-    def duplicate(self, col: int) -> None:
-        for row in self._data:
-            row.insert(col, row[col])
-
-    def transpose(self) -> None:
-        self._data = [[self[i, j] for i in range(self.rows)] for j in range(self.cols)]
-
-
-def chm(terms: list[Term], x: Variable) -> Matrix:
-    if not terms:
-        return Matrix()
-    terms = list(sorted(terms, key=lambda t: t.degree(x)))
-    f = terms.pop()
-    terms.append(f.derivative(x))
-    remainders = []
-    for g in terms:
-        h = real_pseudo_rem(f, g, x)
-        assert h.degree(x) < f.degree(x)
-        remainders.append(h)
-    terms.extend(remainders)
-    constants = list({t for t in terms if t.degree(x) == 0})
-    terms = list({t for t in terms if t.degree(x) > 0})
-    print(f'{len(constants)=}, {constants=}')
-    M = chm(terms, x)
-    # n = max(1, M.cols)
-    # for constant in constants:
-    #     row = n * [sign(constant.as_fraction())]
-    #     M.append(row)
-    return lemma923(term, M)
-
-
-def lemma923(f: Term, G: list[Term], x: Variable) -> Matrix:
-    f_prime = f.derivative(x)
-    f0 = real_pseudo_rem(f, f_prime, x)
-    F = [real_pseudo_rem(f, g, x) for g in G]
-    M = chm([f_prime, *G, f0, *F], x)
-    n = len(G) + 1
-    j = 1
-    stars = []
-    while j < M.cols:
-        for i in range(n):
-            if M[i, 2 * j] == 0:
-                stars.append(M[n + i, 2 * j])
-                j += 2
-                break
+    def extend(self, column: dict[Term, Sign]) -> None:
+        if self.num_rows == 0:
+            self.rows = {f: [s] for f, s in column.items()}
         else:
-            M.delete(j)
-            M.delete(j)
-    return
+            for f, s in column.items():
+                self.rows[f].append(s)
 
 
 def real_pseudo_rem(f: Term, g: Term, x: Variable) -> Term:
@@ -108,3 +52,86 @@ def real_pseudo_rem(f: Term, g: Term, x: Variable) -> Term:
         lc_g_x = g.coefficient({x: g.degree(x)})
         h *= lc_g_x
     return h
+
+
+def lemma923(f: Term, G: set[Term], x: Variable) -> Matrix:
+    assert f not in G
+    assert all(1 <= g.degree(x) <= f.degree(x) for g in G)
+
+    print("call lemma923", f, G)
+
+    g0 = f.derivative(x)
+    h0 = real_pseudo_rem(f, g0, x)
+    H = {g: real_pseudo_rem(f, g, x) for g in G}
+
+    M = chm({g0, *G, h0, *H.values()}, x)
+
+    result = Matrix({f: []} | {g: [] for g in G})
+    lvalue = -M[g0][0]
+
+    for j in range(1, M.num_columns, 2):
+        for g in G:
+            if M[g][j] == 0:
+                rvalue = M[H[g]][j]
+                if lvalue == 0:
+                    result.extend({f: rvalue} | M.column(G, j-1))
+                elif rvalue == 0:
+                    result.extend({f: lvalue} | M.column(G, j-1))
+                elif rvalue == lvalue:
+                    result.extend({f: lvalue} | M.column(G, j-1))
+                else:
+                    result.extend({f: lvalue} | M.column(G, j-1))
+                    result.extend({f: 0}      | M.column(G, j-1))
+                    result.extend({f: rvalue} | M.column(G, j-1))
+                result.extend({f: rvalue} | M.column(G, j))
+                lvalue = rvalue
+                break
+        else:
+            if M[g0][j] == 0 and M[h0][j] == 0:
+                result.extend({f: lvalue} | M.column(G, j-1))
+                result.extend({f: 0}      | M.column(G, j))
+                lvalue = 0
+
+    rvalue = M[g0][-1]
+    if lvalue == 0:
+        result.extend({f: rvalue} | M.column(G, -1))
+    elif rvalue == 0:
+        result.extend({f: lvalue} | M.column(G, -1))
+    elif rvalue == lvalue:
+        result.extend({f: lvalue} | M.column(G, -1))
+    else:
+        result.extend({f: lvalue} | M.column(G, -1))
+        result.extend({f: 0}      | M.column(G, -1))
+        result.extend({f: rvalue} | M.column(G, -1))
+
+
+    print("return lemma923", result)
+    return result
+
+
+def chm(F: set[Term], x: Variable) -> Matrix:
+    print("call chm", F)
+
+    constants = {f for f in F if f.degree(x) <= 0}
+    F = set(F) - constants
+
+    if not F:
+        M = Matrix({})
+    else:
+        largest = max(F, key=lambda f: f.degree(x))
+        M = lemma923(largest, F - {largest}, x)
+
+    for constant in constants:
+        M.insert_constant_row(constant, sign(constant.lc()))
+
+    print("return chm", M)
+    return M
+
+if __name__ == "__main__":
+    x, = VV.get("x")
+    F = {x + 1, 2*x + 1, x**2 - 1}
+    M = chm(F, x)
+    for f in F:
+        print(f, M.row(f))
+
+
