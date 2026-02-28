fb0882b336ac40e4d45937465f0393ececc3eb7d
Thomas Sturm <tsturm@me.com>
2026-02-28 09:26:13 +0100

    src: Provisional code for selecting POLYLIB when importing within Jupyter notebooks

--- logic1/theories/RCF/atomic.py
+++ logic1/theories/RCF/atomic.py
@@ -1,1 +1,16 @@
+# try:
+
+#     import __main__
+
+#     if __main__.choose_polylib == 'SAGE':
+#         from logic1.theories.RCF.atomic_sage import *
+#     elif __main__.choose_polylib == 'FLINT':
+#         from logic1.theories.RCF.atomic_flint import *
+#     else:
+#         raise ValueError(f'illegal value {__main__.choose_polylib=}')
+
+# except AttributeError:
+
+#     from logic1.theories.RCF.atomic_flint import *
+
 from logic1.theories.RCF.atomic_sage import *
