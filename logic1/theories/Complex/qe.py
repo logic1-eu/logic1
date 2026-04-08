4774f5f077e0c673ede22c93d6ebc74e2f209945
Thomas Sturm <tsturm@me.com>
2026-04-08 11:18:11 +0200

    src: Support the use of Redlog in pseudo-complex qe, and improve the use of cnf/dnf computations

--- logic1/theories/Complex/qe.py
+++ logic1/theories/Complex/qe.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Optional
 
 from logic1.firstorder.boolean import _F, _T, And, Equivalent, Implies, Not, Or
 from logic1.firstorder.quantified import All, Ex
@@ -13,30 +13,30 @@ from logic1.theories.Complex.types import Formula
 
 class RCF_Evaluator(ArithmeticEvaluator[RCF.Term]):
     """A term visitor that evaluates a complex term to a term in the
-    theory of real closed fields. Raises a ValueError if the term 
-    contains any complex-specific operations that cannot be evaluated 
+    theory of real closed fields. Raises a ValueError if the term
+    contains any complex-specific operations that cannot be evaluated
     in RCF, such as the imaginary unit or complex conjugation.
     Implements the abstract class :class:`.Complex.ArithmeticEvaluator`.
     """
-    
+
     def _add(self, a: RCF.Term, b: RCF.Term) -> RCF.Term:
         """Returns the sum of the RCF terms a and b. Implements
         abstract method :meth:`.Complex.ArithmeticEvaluator._add`.
         """
         return a + b
-    
+
     def _neg(self, a: RCF.Term) -> RCF.Term:
         """Returns the negation of the RCF term a. Implements the
         abstract method :meth:`.Complex.ArithmeticEvaluator._neg`.
         """
         return -a
-    
+
     def _mul(self, a: RCF.Term, b: RCF.Term) -> RCF.Term:
         """Returns the product of the RCF terms a and b. Implements the
         abstract method :meth:`.Complex.ArithmeticEvaluator._mul`.
         """
         return a * b
-    
+
     def visit_rat(self, num: Rat) -> RCF.Term:
         """Returns the RCF term corresponding to the rational term num.
         Implements the abstract method
@@ -49,31 +49,31 @@ class RCF_Evaluator(ArithmeticEvaluator[RCF.Term]):
         abstract method :meth:`.Complex.TermVisitor.visit_i`.
         """
         raise ValueError("Cannot evaluate imaginary unit in RCF")
-    
+
     def visit_var(self, var: Var) -> RCF.Term:
         """Cannot evaluate complex variables in RCF. Implements the
         abstract method :meth:`.Complex.TermVisitor.visit_var`.
         """
         raise ValueError(f"Cannot evaluate complex variable {var} in RCF")
-    
+
     def visit_conj(self, conj: Conj) -> RCF.Term:
         """Cannot evaluate complex conjugations in RCF. Implements the
         abstract method :meth:`.Complex.TermVisitor.visit_conj`.
         """
         raise ValueError(f"Cannot evaluate complex conjunction {conj} in RCF")
-    
+
     def visit_re(self, re: Re) -> RCF.Term:
         if isinstance(re.arg, Var):
             return RCF.VV[f"{re.arg.name}_re"]
         else:
             raise ValueError(f"Cannot evaluate real part of non-variable term {re.arg} in RCF")
-        
+
     def visit_im(self, im: Im) -> RCF.Term:
         if isinstance(im.arg, Var):
             return RCF.VV[f"{im.arg.name}_im"]
         else:
             raise ValueError(f"Cannot evaluate imaginary part of non-variable term {im.arg} in RCF")
-        
+
 def real_atom_to_rcf(atom: AtomicFormula) -> RCF.AtomicFormula:
     """Converts an atomic formula in the theory of complex numbers that is
     already known to be a real formula to an equivalent atomic formula
@@ -93,7 +93,7 @@ def real_atom_to_rcf(atom: AtomicFormula) -> RCF.AtomicFormula:
         return RCF.Le(lhs, 0)
     elif isinstance(atom, Lt):
         return RCF.Lt(lhs, 0)
-    else:                
+    else:
         assert False, type(atom)
 
 def formula_to_rcf(formula: Formula) -> RCF.Formula:
@@ -103,7 +103,7 @@ def formula_to_rcf(formula: Formula) -> RCF.Formula:
     that cannot be evaluated in RCF, such as the imaginary unit or
     complex conjugation.
     """
-    if isinstance(formula, AtomicFormula): 
+    if isinstance(formula, AtomicFormula):
        if formula.is_real():
             return real_atom_to_rcf(formula)
        else:
@@ -163,9 +163,9 @@ def formula_to_complex(formula: RCF.Formula) -> Formula:
     if isinstance(formula, (And, Or, Not, Implies, Equivalent, _T, _F)):
         return formula.op(*(formula_to_complex(arg) for arg in formula.args))
     assert False, type(formula)
-    
 
-def qe(formula: Formula, assume: Iterable[AtomicFormula] = [], **options) -> Formula:
+
+def qe(formula: Formula, assume: Iterable[AtomicFormula] = [], use_redlog: bool = False, **options) -> Formula:
     """Quantifier elimination for the theory of complex numbers. Returns a
     quantifier-free formula equivalent to the input formula.
     """
@@ -173,19 +173,26 @@ def qe(formula: Formula, assume: Iterable[AtomicFormula] = [], **options) -> For
     if isinstance(assumption, RCF.AtomicFormula):
         rcf_assume = [assumption]
     elif isinstance(assumption, And):
-        rcf_assume = [arg for arg in assumption.args 
+        rcf_assume = [arg for arg in assumption.args
                       if isinstance(arg, RCF.AtomicFormula)]
     else:
         rcf_assume = []
     rcf_formula = formula_to_rcf(formula).to_pnf()
     topmost_op = type(rcf_formula)
-    rcf_qe_formula = RCF.qe(rcf_formula, assume=rcf_assume, **options)
+    if use_redlog:
+        rcf_qe_formula: Optional[RCF.Formula] = RCF.redlog.qe(rcf_formula, assume=rcf_assume)
+    else:
+        rcf_qe_formula = RCF.qe(rcf_formula, assume=rcf_assume, **options)
     if rcf_qe_formula is None:
         raise ValueError("Quantifier elimination failed")
+    # TS: I had suggested to look at rcf_qe_formula.op, but now I think it is
+    # better to stick with checking the topmost_op. When in doubt, DNF should be
+    # used. The double calls of CNF/DNF are probably the best solution at the
+    # moment. We can discuss this.
     if topmost_op == All:
-        rcf_qe_formula = RCF.simplify(RCF.cnf(rcf_qe_formula))
-    elif topmost_op == Ex:
-        rcf_qe_formula = RCF.simplify(RCF.dnf(rcf_qe_formula))
+        rcf_qe_formula = RCF.cnf(RCF.cnf(rcf_qe_formula))
+    else:
+        rcf_qe_formula = RCF.dnf(RCF.dnf(rcf_qe_formula))
     result = formula_to_complex(rcf_qe_formula)
     result = simplify(result)
     return result
