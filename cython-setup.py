664187436e0d5b88379a7659e11a07e2fc6f1cfa
Thomas Sturm <tsturm@me.com>
2026-04-14 14:24:57 +0200

    build: Remove references to local directories

--- cython-setup.py
+++ cython-setup.py
@@ -3,10 +3,12 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize([Extension(
-    'logic1.theories.RCF.range',
-    sources=['logic1/theories/RCF/range.pyx'],
-    include_dirs=['/Users/sturm/miniforge3/envs/logic1_dev/lib/python3.12/site-packages/gmpy2'],
-    # extra_compile_args=['-DCYTHON_WITHOUT_ASSERTIONS'],
-    )],
-    annotate=True))
+setup(
+    ext_modules=cythonize([
+        Extension(
+            'logic1.theories.RCF.range',
+            sources=['logic1/theories/RCF/range.pyx'],
+        )
+    ],
+    annotate=True)
+)
